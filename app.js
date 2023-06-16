0c67d6ba171afb3fb322a19ce106fe71bd548289
yavnek <jonatan.gasiorowski@gmail.com>
2023-06-16 19:45:09 +0200

    template engines

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var nataliaRouter = require('./routes/natalia');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -15,7 +16,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/natalia', nataliaRouter);
-
+app.use('/about', aboutRouter);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -25,6 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Serve a single static file
+app.use('/file', express.static(path.join(__dirname, 'public/file.txt')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
